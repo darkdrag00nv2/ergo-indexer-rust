43aa6717c0838a54c1ba6508ff21a0a40708d734
darkdrag00nv2 <darkdrag00n@rambler.ru>
2023-03-24 18:57:11 +0530

    Fix formatting

--- api/app.ts
+++ api/app.ts
@@ -11,17 +11,17 @@ const app = new Application();
 
 // Logger
 app.use(async (ctx, next) => {
-    await next();
-    const rt = ctx.response.headers.get("X-Response-Time");
-    log.info(`${ctx.request.method} ${ctx.request.url} - ${rt}`);
+  await next();
+  const rt = ctx.response.headers.get("X-Response-Time");
+  log.info(`${ctx.request.method} ${ctx.request.url} - ${rt}`);
 });
 
 // Timing
 app.use(async (ctx, next) => {
-    const start = Date.now();
-    await next();
-    const ms = Date.now() - start;
-    ctx.response.headers.set("X-Response-Time", `${ms}ms`);
+  const start = Date.now();
+  await next();
+  const ms = Date.now() - start;
+  ctx.response.headers.set("X-Response-Time", `${ms}ms`);
 });
 
 app.use(router.routes());
--- api/deps.ts
+++ api/deps.ts
@@ -1,2 +1,2 @@
 import { Application, Router } from "https://deno.land/x/oak/mod.ts";
-export { Router, Application };
+export { Application, Router };
